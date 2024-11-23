7645db350aaf28a9b3684e93508ee15469315891
juyeon Bae <w2316@e-mirim.hs.kr>
2024-11-23 12:31:34 +0900

    feat : 정보 담아놓음(틀만)

---
+++ public/js/solveInfo.js
@@ -0,0 +1,19 @@
+var solveInfo = [
+    {
+        "mainTitle": "내 적성과 역량 알아보기",
+        "subTitle": ["", ""],
+        "detailTitle": ["", ""],
+    },
+
+    {
+        "mainTitle": "내 적성과 역량 알아보기",
+        "subTitle": ["", ""],
+        "detailTitle": ["", ""],
+    },
+    {
+        "mainTitle": "내 적성과 역량 알아보기",
+        "subTitle": ["", ""],
+        "detailTitle": ["", ""],
+    },
+
+]
