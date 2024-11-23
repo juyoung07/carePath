d1f965bf188a5cb96fdcae18fa75f6e08bdb4362
juyeon Bae <w2316@e-mirim.hs.kr>
2024-11-24 04:49:21 +0900

    feat : 원터치 바우처 페이지 구현

--- public/js/voucher.js
+++ public/js/voucher.js
@@ -74,3 +74,38 @@ voucher.forEach(item => {
     questionContainer.appendChild(subTitleContainer)
     container.appendChild(questionContainer)
 })
+
+
+const popup = document.querySelector('.popup-container');
+const overlay = document.querySelector('.overlay');
+const rightArrows = document.querySelectorAll('.worryList-container');
+const closeButton = document.querySelector('.x-icon')
+// 오른쪽 화살표 클릭 시 팝업 열기
+rightArrows.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+        overlay.classList.remove('hidden');
+        popup.classList.remove('hidden');
+    });
+});
+
+// X 아이콘 클릭 시 팝업 닫기
+closeButton.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+    popup.classList.add('hidden');
+});
+
+const printButton = document.querySelector('.contact-button')
+
+printButton.addEventListener('click', () => {
+    window.location.href = "#" //print하는 페이지로 넘어가기
+})
+
+function printPage() {
+    window.print();
+    setTimeout(() => {
+        window.location.href = "/views/counselor/infoPrint.html";
+    }, 500);
+}
+
+
+
